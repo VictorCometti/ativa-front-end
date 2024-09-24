096b56d8dc4d0016d662df574d43c0452f25a40d
Victor Cometti <victorhugoocometti@gmail.com>
2024-09-24 20:33:24 -0300

    create item button

---
+++ front-end/src/app/components/item-button/item-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-item-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-button.component.html',
+  styleUrl: './item-button.component.scss'
+})
+export class ItemButtonComponent {
+  public click = output<void>();
+  public buttonName = input.required<string>();
+  protected buttonClick() {
+    this.click.emit();
+  }
+}
