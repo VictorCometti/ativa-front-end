1bca4d713c61a6f4305921e984b9d712d8dba64c
Victor Cometti <victorhugoocometti@gmail.com>
2024-09-24 20:33:43 -0300

    create header options

--- front-end/src/app/components/header-options/menu-options.ts
+++ front-end/src/app/components/header-options/menu-options.ts
@@ -1,6 +1,6 @@
 export const menuOptions = [
     {name:"PRODUTOS", icon:"fa-solid fa-shield"},
-    {name:"SOLICITE ORÇAMENTO", icon:"fa-solid fa-file"},
-    {name:"ENTRE EM CONTATO", icon:"fa-solid fa-phone"}
+    {name:"ORÇAMENTO", icon:"fa-solid fa-file"},
+    {name:"CONTATO", icon:"fa-solid fa-phone"}
 
 ];
