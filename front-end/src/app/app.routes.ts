1e813059e3e8590615dca0e2ca57b1ff64479a7e
Victor Cometti <victorhugoocometti@gmail.com>
2024-10-03 11:26:04 -0300

    configure routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CategoryPageComponent } from './pages/product-page/category-page.component';
 
 export const routes: Routes = [
 
     {
         path: '',
-        component: HomePageComponent
-    }
+        component: HomePageComponent,
+        data: { scrollPositionRestoration: 'enabled' }
+    },
+    {
+        path: 'product_category/:category',
+        component: CategoryPageComponent,
+        data: { scrollPositionRestoration: 'enabled' }
+    },
 ];
