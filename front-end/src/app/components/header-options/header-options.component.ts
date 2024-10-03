87afa6afd7adc24bf71b9152cae41095420427cf
Victor Cometti <victorhugoocometti@gmail.com>
2024-10-03 11:19:12 -0300

    Create header options

--- front-end/src/app/components/header-options/header-options.component.ts
+++ front-end/src/app/components/header-options/header-options.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { menuOptions } from './menu-options';
+import { CommonModule } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { NavigateService } from '../../services/navigate.service';
+import { ScrollService } from '../../services/scroll.service';
+import { menuOptions, productMenu } from './menu-options';
 
 @Component({
   selector: 'app-header-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-options.component.html',
   styleUrl: './header-options.component.scss',
 })
 export class HeaderOptionsComponent {
   protected menuOptions = menuOptions;
+  public isProductPage = input.required<boolean>();
+  protected productOptions = productMenu;
+  protected navigateService = inject(NavigateService);
+  protected scrollService = inject(ScrollService);
 }
--- front-end/src/app/components/header-options/menu-options.ts
+++ front-end/src/app/components/header-options/menu-options.ts
@@ -1,6 +1,11 @@
 export const menuOptions = [
-    {name:"PRODUTOS", icon:"fa-solid fa-shield"},
-    {name:"ORÇAMENTO", icon:"fa-solid fa-file"},
-    {name:"CONTATO", icon:"fa-solid fa-phone"}
+    {name:"PRODUTOS", icon:"fa-solid fa-shield", goTo:"product-section"},
+    {name:"ORÇAMENTO", icon:"fa-solid fa-file", goTo:"budget-section"},
+    {name:"CONTATO", icon:"fa-solid fa-phone", goTo:"about-section"},
+];
 
+export const productMenu = [
+    {name:"PÁGINA INICIAL", icon:'fa-solid fa-house', goTo:''},
+    {name:"ORÇAMENTO", icon:"fa-solid fa-file", goTo:"budget-section"},
+    {name:"CONTATO", icon:"fa-solid fa-phone", goTo:"about-section"},
 ];
