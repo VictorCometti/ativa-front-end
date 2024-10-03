2878f795e419d5cdb85795451a7f1e16e3948b93
Victor Cometti <victorhugoocometti@gmail.com>
2024-10-03 11:19:31 -0300

    create initial section

--- front-end/src/app/components/initial-content/initial-content.component.ts
+++ front-end/src/app/components/initial-content/initial-content.component.ts
@@ -1,68 +1,20 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { slideInAnimationItem, slideInDelayAnimation } from '../../animations/slide-in';
+import { ScrollService } from '../../services/scroll.service';
+import { DownButtonComponent } from "../down-button/down-button.component";
 
 @Component({
   selector: 'app-initial-content',
   standalone: true,
-  imports: [],
+  imports: [DownButtonComponent],
   templateUrl: './initial-content.component.html',
   styleUrl: './initial-content.component.scss',
   animations: [
-    trigger('slideIn', [
-      transition(':enter', [
-        style({ transform: 'translateX(100%)', opacity: 0 }), // posição inicial
-        animate('0.6s ease-out', style({ transform: 'translateX(0)', opacity: 1 })) // animação de entrada
-      ])
-    ]),
-    trigger('slideInDelay', [
-      transition(':enter', [
-        style({ transform: 'translateX(100%)', opacity: 0 }),
-        animate('0.8s ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
-      ])
-    ]),
-    trigger('slideInButton', [
-      state('hidden', style({
-        transform: 'translateX(100%)',
-        opacity: 0,
-      })),
-      state('visible', style({
-        transform: 'translateX(0)',
-        opacity: 1,
-      })),
-      transition('hidden => visible', [
-        animate('1s ease-out')
-      ]),
-    ]),
-    trigger('hoverGrow', [
-      state('normal', style({
-        transform: 'scale(1)'
-      })),
-      state('grow', style({
-        transform: 'scale(1.01)',
-        boxShadow: '0px 0px 10px 1px rgba(0, 0, 0, 0.2)'
-      })),
-      transition('normal <=> grow', [
-        animate('0.1s ease-in-out')
-      ])
-    ])
+    slideInAnimationItem,
+    slideInDelayAnimation
   ]
-
 })
 export class InitialContentComponent {
-  buttonState = 'hidden';
-  hoverState = 'normal';
+  protected scrollService = inject(ScrollService);
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.buttonState = 'visible';
-    }, 300);
-  }
-
-  onMouseEnter() {
-    this.hoverState = 'grow';
-  }
-
-  onMouseLeave() {
-    this.hoverState = 'normal';
-  }
 }
